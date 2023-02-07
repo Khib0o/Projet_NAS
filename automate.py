164e5920ebdda92f2a08d4611584dce5c9df4a38
yssine <yassine.lahmer120@gmail.com>
2023-02-08 00:22:21 +0100

    'NAS'

---
+++ automate.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python3
+import os, json, glob, platform, re, jinja2
+
+
+GNSFILE='Projet2.gns3'
+router_path={}
+hosts=[]
+
+f=open(GNSFILE)
+raw=json.load(f)
+# raw=f.read()
+# print(raw.keys())
+for node in raw['topology']['nodes']:
+    path=r'project-files/dynamips/'+node['node_id']+r'/configs/'
+    conf_file=glob.glob(path+'/*startup*',recursive=True)[0]
+    if platform.uname().system=='Windows':
+        conf_file=conf_file.split('\\')
+        # os.system(f"python prep.py {GNSFILE}")
+    else:
+        conf_file=conf_file.split('/')
+        # os.system(f"python3 prep.py {GNSFILE}")
+    conf_file=conf_file[-1]
+    # print(conf_file)
+    hosts.append(node['name'])
+    router_path[node['name']]= path+conf_file
+f.close()
+# print(router_path)
+
+
+class Router:
+    def __init__(self, name, input):
+        self.name       = name
+        if self.name in router_path.keys():
+            self.path   = router_path[self.name]
+        self.input      = input
+        self.cl      = self.input["Routers"][self.name]["class"]
+        self.RP         = self.input["Routers"][self.name]["RP"]
+        if "BGP" in self.RP:
+            self.BGP   = self.input["Routers"][self.name]["BGP"]
+        if "VPN" in self.RP:
+            self.VPN   = self.input["Routers"][self.name]["VPN"]
+        if "OSPF" in self.RP:
+            self.OSPF   = self.input["Routers"][self.name]["OSPF"]
+        self.itfs = self.fill_ints()
+
+    def fill_ints(self):
+        ints = []
+        for interface in self.input["Routers"][self.name]["ints"]:
+            # print(interface)
+            n = self.input["Links"][interface["link"]][self.name]
+            if not "VRF" in interface.keys():
+                interface["VRF"]=False
+            ints.append(Interface(interface["link"],n,interface["IP"],interface["mask_l"],interface["VRF"]))
+        return ints
+
+
+class Interface:
+    def __init__(self, link, name, ip, mask_l,VRF):
+        self.link       = link
+        self.name       = name
+        self.ip         = ip
+        self.mask_l     = mask_l
+        self.mask       = self.mask(self.mask_l)
+        self.VRF        = VRF
+
+    def mask(self,l):
+            s = '1'*l
+            s = s.ljust(32,'0')
+            my_mask = ""
+            for i in range(4):
+                my_mask = my_mask + str(int(s[i*8:(i+1)*8],2)) + '.'
+            return my_mask[:-1]
+
+
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader('template'))
+
+def prep():
+    if platform.uname().system=='Windows':
+        os.system(f"python prep.py {GNSFILE}")
+    else:
+        os.system(f"python3 prep.py {GNSFILE}")
+
+
+def auto(RX):
+    f=open('inpt.json')
+    raw=json.load(f)
+    Rx=Router(RX,raw)
+    jinja_var = {
+        'R'         : Rx
+    }
+
+    template = jinja_env.get_template('genconf.txt')
+    conf=template.render(jinja_var)
+    # print(conf)
+    # print(Rx.RP)
+    # print(hosts)
+    # print(Rx.name)
+
+    s=open(router_path[Rx.name],"w")
+    # s=open(Rx.name+'.cfg',"w")
+    s.write(conf)
+    s.close()
+
+
+# prep()
+# auto("R11")
+for h in hosts:
+    auto(h)
---
+++ prep.py
@@ -0,0 +1,280 @@
+#!/usr/bin/python3
+import os, json, glob, platform, re, sys
+
+try:
+    GNSFILE=sys.argv[1]
+except :
+    GNSFILE='Projet2.gns3'
+
+print('Preparing the input file......')
+
+f=open(GNSFILE)
+raw=json.load(f)
+f.close()
+# # raw=f.read()
+class Link:
+    def __init__(self, name, id, term1, term2):
+        self.name    = name
+        self.id      = id
+        self.term1   = term1
+        self.term2   = term2
+        self.network = ""
+
+class Term:
+    def __init__(self, Router_name, Router_id, inter):
+        self.Router_name = Router_name
+        self.Router_id = Router_id
+        self.inter = inter
+
+
+
+
+def surf(raw):
+    Router_id = {}
+    LINKS = {}
+    for node in raw["topology"]["nodes"]:
+        Router_id[node["node_id"]]=node["name"]
+    count=1
+    for link in raw["topology"]["links"]:
+        LINKS[f'L{count}']=Link(f'L{count}',link["link_id"],Term(Router_id[link["nodes"][0]["node_id"]],link["nodes"][0]["node_id"],link["nodes"][0]['label']['text']),Term(Router_id[link["nodes"][1]["node_id"]],link["nodes"][1]["node_id"],link["nodes"][1]['label']['text']))
+        count=count+1
+    return LINKS, Router_id
+
+# print(surf(raw))
+
+class Router:
+    def __init__(self, rid, name, intfs):
+        self.id      = rid
+        self.name    = name
+        self.intfs   = intfs
+
+    def add_ints(self,link,name,ip,mask):
+        self.intfs[name]=Inter(link,name,ip,mask)
+
+
+class Inter:
+    def __init__(self, link, name, ip, mask):
+        self.link    = link
+        self.name    = name
+        self.ip      = ip
+        self.mask    = mask
+
+
+
+# f=open('inpt.json')
+# raw=json.load(f)
+# f.close()
+def DHCP(inp):
+    NET_PREF="192.186."
+    Ls,Rid=inp
+    Rs={}
+    for R in Rid:
+        Rs[R]=Router(R,Rid[R],{})
+
+    for router in Rs:
+        l0=(Rs[router].name[1:]+'.')*4
+        Rs[router].add_ints('L0',"loopback0",l0[:-1],32)
+        # print(l0[:-1])
+
+
+    for L in Ls.values():
+        subnet = L.name[1:]
+        c=1
+        L.network = NET_PREF+f"{subnet}.0"
+        # print(L.name,L.network)
+        for term in [L.term1,L.term2]:
+            ip=L.network[:-1]+str(c)
+            Rs[term.Router_id].add_ints(L.name,term.inter,ip,24)
+            c=c+1
+            # print(Rs[term.Router_id].name,term.inter,ip)
+
+    # print(Rs['eb4420b5-8862-44ba-a069-9b3b6d192a11'].intfs["f2/0"].ip)
+
+    return Rs
+
+
+Routing_Protocols={"CE":["BGP"],"PE":["BGP","OSPF","VPN"],"P":["OSPF"]}
+
+def CEs():
+    f=open("descriptor.json")
+    d=json.load(f)
+    f.close()
+    CE=[]
+    Cust=[j for j in [d[i] for i in d.keys() if i[:2]=="CE"]]
+    for k in Cust:
+        for l in k:
+            CE.append(l)
+    return CE
+# print(CEs(d))
+
+Rs=DHCP(surf(raw))
+
+
+def cl(name):
+    f=open("descriptor.json")
+    d=json.load(f)
+    f.close()
+    if name in CEs():
+        return "CE"
+    elif name in d["PE"]:
+        return "PE"
+    elif name in d["P"]:
+        return "P"
+
+def vpn_ls():
+    f=open("descriptor.json")
+    d=json.load(f)
+    f.close()
+    vpn=[]#[{"VPN_id": i+1,"RD":f"{i+1}:{i+1}","RT":f"{i+1}:{i+1}"} for i in range(len(d["Clients"]))]
+    for i,l in enumerate(d["Clients"]):
+        v={}
+        i=i+1
+        v["VPN_id"]=i
+        v["RD"]=f"{i}:{i}"
+        v["RT"]=f"{i}:{i}"
+        v["concerned_PEs"]=[]
+        for c in d[l]:
+            for link in surf(raw)[0].values():
+                if c ==[link.term1.Router_name,link.term2.Router_name][0]:
+                    v["concerned_PEs"].append(([link.term1.Router_name,link.term2.Router_name][1],link))
+                if c ==[link.term1.Router_name,link.term2.Router_name][1]:
+                    v["concerned_PEs"].append(([link.term1.Router_name,link.term2.Router_name][0],link))
+        vpn.append(v)
+    return vpn
+
+def bgp():
+    f=open("descriptor.json")
+    d=json.load(f)
+    f.close()
+    links=surf(raw)[0].values()
+    # Rs=DHCP(surf(raw))
+    b={"PE":{"BGP_AS":1,"neighbor":[]},"CE":{},"CEp":{}}
+    c=2
+    for rout in Rs.values():
+        if rout.name in d["PE"]:
+            b["PE"]["neighbor"].append(([rout.intfs["loopback0"].ip,1],rout.name))
+        # print(CEs())
+        if rout.name in CEs():
+            b["CE"][rout.name]={"BGP_AS":c,"neighbor":[]}
+            b["CEp"][rout.name]={"BGP_AS":c,"neighbor":[]}
+            for l in links:
+                if rout.name==l.term1.Router_name:
+                    b["CE"][rout.name]["neighbor"].append(([rout.intfs[l.term1.inter].ip,c],l.term2.Router_name))
+                    b["CEp"][rout.name]["neighbor"].append(([Rs[l.term2.Router_id].intfs[l.term2.inter].ip,1],l.term2.Router_name))
+                if rout.name==l.term2.Router_name:
+                    b["CE"][rout.name]["neighbor"].append(([rout.intfs[l.term2.inter].ip,c],l.term1.Router_name))
+                    b["CEp"][rout.name]["neighbor"].append(([Rs[l.term1.Router_id].intfs[l.term1.inter].ip,1],l.term1.Router_name))
+            c=c+1
+    # print(b)
+    return b
+
+# print(bgp())
+# bgp()
+
+# print(vpn_ls()[0])
+# print(cl("R15"))
+
+# for i in Rs:
+#     print(Rs[i].name,':')
+#     for x in Rs[i].intfs:
+#         print("\t",x,':')
+#         print("\t"*2,Rs[i].intfs[x].ip,'/',Rs[i].intfs[x].mask)
+def generate():
+    input={}
+    input["Links"],input["Routers"]={},{}
+    # print(dir(surf(raw)[0]["L1"]))
+    infos = surf(raw)
+    input["Links"]["L0"]={}
+    for j in infos[1]:
+        input["Links"]["L0"][infos[1][j]]="loopback0"
+    for i in infos[0]:
+        input["Links"][i]={}
+        # print(dir(infos[0][i].term1))
+        input["Links"][i][infos[0][i].term1.Router_name]=infos[0][i].term1.inter
+        input["Links"][i][infos[0][i].term2.Router_name]=infos[0][i].term2.inter
+    for r in Rs.values():
+        input["Routers"][r.name]={}
+        input["Routers"][r.name]["name"]=r.name
+        input["Routers"][r.name]["class"]=cl(r.name)
+        input["Routers"][r.name]["RP"]=Routing_Protocols[cl(r.name)]
+        if "VPN" in Routing_Protocols[cl(r.name)]:
+            input["Routers"][r.name]["VPN"]=[]
+            for v in vpn_ls():
+                if r.name in [i[0] for i in v["concerned_PEs"]] :
+                    smth={}
+                    smth["VPN_id"]=v["VPN_id"]
+                    smth["RD"]=v["RD"]
+                    smth["RT"]=[v["RT"]]
+                    smth["neighbor"]=[]
+                    for k in bgp()["CE"].values():
+                        consL=[i for i in v["concerned_PEs"] if i[0]==r.name][0][1]
+                        consIP=[Rs[consL.term1.Router_id].intfs[consL.term1.inter].ip,Rs[consL.term2.Router_id].intfs[consL.term2.inter].ip]
+                        # print(consIP)
+                        # print(k["neighbor"])
+                        # print(k["neighbor"][0][0][0]==v["concerned_PEs"])
+                        if k["neighbor"][0][1]==r.name and k["neighbor"][0][0][0] in consIP:
+                            smth["neighbor"].append(k["neighbor"][0][0])  #[["ip?","remote_as(int)"]]
+                            # print()
+                    input["Routers"][r.name]["VPN"].append(smth)
+        if "BGP" in Routing_Protocols[cl(r.name)]:
+            input["Routers"][r.name]["BGP"]={}
+            if cl(r.name)=="PE":
+                input["Routers"][r.name]["BGP"]["BGP_AS"]=bgp()["PE"]["BGP_AS"]
+                input["Routers"][r.name]["BGP"]["neighbor"]=[]
+                for n in bgp()["PE"]["neighbor"]:
+                    if r.name != n[1]:
+                        input["Routers"][r.name]["BGP"]["neighbor"].append(n[0])
+            if cl(r.name)=="CE":
+                input["Routers"][r.name]["BGP"]["BGP_AS"]=bgp()["CEp"][r.name]["BGP_AS"]
+                input["Routers"][r.name]["BGP"]["neighbor"]=[]
+                # print(bgp()["CEp"][r.name]["neighbor"][0][0])
+                input["Routers"][r.name]["BGP"]["neighbor"].append(bgp()["CEp"][r.name]["neighbor"][0][0])
+        if "OSPF" in Routing_Protocols[cl(r.name)]:
+                f=open("descriptor.json")
+                d=json.load(f)
+                f.close()
+                input["Routers"][r.name]["OSPF"]=d["OSPF"]
+
+
+
+        input["Routers"][r.name]["ints"]=[]
+        for intf in r.intfs.values():
+            i={}
+            i["link"]=intf.link
+            # print(intf.link)
+            i["IP"]=intf.ip
+            i["mask_l"]=intf.mask
+            if cl(r.name) == "PE":
+                for v in vpn_ls():
+                    a=list(zip([i[1].term1.inter for i in v["concerned_PEs"]],[i[1].term1.Router_name for i in v["concerned_PEs"]]))
+                    b=list(zip([i[1].term2.inter for i in v["concerned_PEs"]],[i[1].term2.Router_name for i in v["concerned_PEs"]]))
+                    # print((intf.name,r.name),a)
+                    if (intf.name,r.name) in a or (intf.name,r.name) in b:
+                        i["VRF"]=v["VPN_id"]
+            input["Routers"][r.name]["ints"].append(i)
+
+
+
+
+    json_object = json.dumps(input, indent = 4)
+    # print(type(json_object))
+    # print(input)
+    f=open("inpt.json","w")
+    f.write(json_object)
+    f.close()
+
+
+
+generate()
+# for i in Rs.values():
+#     for j in i.intfs.values():
+#         print(j.link)
+
+# for l in surf(raw)[0].values():
+#         couple1=(Rs[l.term1.Router_id].name,l.term1.inter)
+#         ip1= Rs[l.term1.Router_id].intfs[l.term1.inter].ip
+#         ip2= Rs[l.term2.Router_id].intfs[l.term2.inter].ip
+#         couple2=(Rs[l.term2.Router_id].name,l.term2.inter)
+        # print(l.network,':\t',couple1,":",ip1,'\t',couple2,':',ip2)
+        # print(l)
+
